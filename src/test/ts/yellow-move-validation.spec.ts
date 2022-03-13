2db9af88d36c87b63d6c3b36833e8eb81648bdd7
= <monique.rimbert@etu.univ-nantes.fr>
2022-03-13 14:56:02 +0100

    All tests for pawns in yellow squares movements

--- src/test/ts/yellow-move-validation.spec.ts
+++ src/test/ts/yellow-move-validation.spec.ts
@@ -1,51 +1,68 @@
 import { Expect, Test, Setup } from 'alsatian';
+import * as pieces from '../../main/ts/piece';
 import { Chessboard, createEmptyChessboard, putPiece } from '../../main/ts/chessboard';
+import * as position from '../../main/ts/position';
+import { isMovePossible, Move, move } from "../../main/ts/movements";
 
 let chessboard: Chessboard;
+
+// Valid movements
+const D3_F4 : Move = move(position.D3, position.F4);
+const D3_C5 : Move = move(position.D3, position.C5);
+
+// Invalid movements
+const D3_F3 : Move = move(position.D3, position.F3);
+const D3_D5 : Move = move(position.D3, position.D5);
+const D3_F5 : Move = move(position.D3, position.F5);
 
 export class TestPawnInYellowSquareMoves {
     @Setup
     beforeEach(): void {
-        // TODO:
+        // For all test squares, prepare an empty board and put a White bishop in D3 (yellow square)
+        chessboard = createEmptyChessboard();
+        putPiece(chessboard, position.D3, pieces.whitePawn);
     }
 
     @Test('In a yellow square, a Pawn can move two squares horizontally and one square vertically')
     testCanMoveThreeHorizontalAndOneVertical(): void {
-        // TODO:
+        Expect(isMovePossible(chessboard, D3_F4)).toBeTruthy();
     }
 
     @Test('In a yellow square, a Pawn can move two squares vertically and one square horizontally')
     testCanMoveThreeVerticalAndOneHorizontal(): void {
-        // TODO:
+        Expect(isMovePossible(chessboard, D3_C5)).toBeTruthy();
     }
 
     @Test('In a yellow square, a Pawn can leap other pieces')
     testCanLeapOtherPieces(): void {
-        // TODO:
+        putPiece(chessboard, position.E3, pieces.blackPawn);
+        Expect(isMovePossible(chessboard, D3_F4)).toBeTruthy();
     }
 
     @Test('In a yellow square, a Pawn cannot move diagonally')
     testCannotMoveDiagonally(): void {
-        // TODO:
+        Expect(isMovePossible(chessboard, D3_F5)).not.toBeTruthy();
     }
 
     @Test('In a yellow square, a Pawn cannot move horizontally')
     testCannotMoveHorizontally(): void {
-        // TODO:
+        Expect(isMovePossible(chessboard, D3_F3)).not.toBeTruthy();
     }
 
     @Test('In a yellow square, a Pawn cannot move vertically')
     testCannotMoveVertically(): void {
-        // TODO:
+        Expect(isMovePossible(chessboard, D3_D5)).not.toBeTruthy();
     }
 
     @Test('In a yellow square, a Pawn can capture a piece from another color')
     testCanCaptureAnotherColor(): void {
-        // TODO:
+        putPiece(chessboard, position.F4, pieces.blackPawn);
+        Expect(isMovePossible(chessboard, D3_F4)).toBeTruthy();
     }
 
     @Test('In a yellow square, a Pawn cannot capture a piece from the same color')
     testCannotCaptureSameColor(): void {
-        // TODO:
+        putPiece(chessboard, position.F4, pieces.whitePawn);
+        Expect(isMovePossible(chessboard, D3_F4)).not.toBeTruthy();
     }
 }
