7dd88f97aafa20c86e1911121048166e19a460eb
= <monique.rimbert@etu.univ-nantes.fr>
2022-03-14 14:50:34 +0100

    Movements added

--- src/test/ts/yellow-move-validation.spec.ts
+++ src/test/ts/yellow-move-validation.spec.ts
@@ -7,8 +7,17 @@ import { isMovePossible, Move, move } from "../../main/ts/movements";
 let chessboard: Chessboard;
 
 // Valid movements
+    // Two horizontal - One vertical
 const D3_F4 : Move = move(position.D3, position.F4);
+const D3_F2 : Move = move(position.D3, position.F2);
+const D3_B4 : Move = move(position.D3, position.B4);
+const D3_B2 : Move = move(position.D3, position.B2);
+
+    // One horizontal - Two vertical
 const D3_C5 : Move = move(position.D3, position.C5);
+const D3_E5 : Move = move(position.D3, position.E5);
+const D3_C1 : Move = move(position.D3, position.C1);
+const D3_E1 : Move = move(position.D3, position.E1);
 
 // Invalid movements
 const D3_F3 : Move = move(position.D3, position.F3);
@@ -25,12 +34,12 @@ export class TestPawnInYellowSquareMoves {
 
     @Test('In a yellow square, a Pawn can move two squares horizontally and one square vertically')
     testCanMoveThreeHorizontalAndOneVertical(): void {
-        Expect(isMovePossible(chessboard, D3_F4)).toBeTruthy();
+        Expect(isMovePossible(chessboard, D3_B2)).toBeTruthy();
     }
 
     @Test('In a yellow square, a Pawn can move two squares vertically and one square horizontally')
     testCanMoveThreeVerticalAndOneHorizontal(): void {
-        Expect(isMovePossible(chessboard, D3_C5)).toBeTruthy();
+        Expect(isMovePossible(chessboard, D3_E1)).toBeTruthy();
     }
 
     @Test('In a yellow square, a Pawn can leap other pieces')
